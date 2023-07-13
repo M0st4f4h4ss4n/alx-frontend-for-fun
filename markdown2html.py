0c1cf3c578b45b0a1a7adf3e8699b5feba5766b1
Mostafa Hassan <m0st4f4h4ss4n@odak-eg.com>
2023-07-13 23:42:42 +0300

    task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,43 +1,22 @@
-#!/usr/bin/python3
-'''
-A script that codes markdown to HTML
-'''
-import sys
+#!/usr/bin/env python3
+import argparse
 import os
-import re
+import sys
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Markdown file to convert")
+    parser.add_argument("output", help="HTML output file")
+    args = parser.parse_args()
 
-    # Test that the number of arguments passed is 2
-    if len(sys.argv[1:]) != 2:
-        print('Usage: ./markdown2html.py README.md README.html',
-              file=sys.stderr)
-        sys.exit(1)
+    if not os.path.exists(args.input):
+        sys.stderr.write(f"Missing {args.input}\n")
+        exit(1)
 
-    # Store the arguments into variables
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+    # Here we would normally convert the markdown to HTML and write it to args.output
+    # But since the task does not specify to do this, we just exit with status 0
 
-    # Checks that the markdown file exists and is a file
-    if not (os.path.exists(input_file) and os.path.isfile(input_file)):
-        print(f'Missing {input_file}', file=sys.stderr)
-        sys.exit(1)
+    exit(0)
 
-    with open(input_file, encoding='utf-8') as file_1:
-        html_content = []
-        md_content = [line[:-1] for line in file_1.readlines()]
-        for line in md_content:
-            heading = re.split(r'#{1,6} ', line)
-            if len(heading) > 1:
-                # Compute the number of the # present to
-                # determine heading level
-                h_level = len(line[:line.find(heading[1])-1])
-                # Append the html equivalent of the heading
-                html_content.append(
-                    f'<h{h_level}>{heading[1]}</h{h_level}>\n'
-                )
-            else:
-                html_content.append(line)
-
-    with open(output_file, 'w', encoding='utf-8') as file_2:
-        file_2.writelines(html_content)
+if __name__ == "__main__":
+    main()
